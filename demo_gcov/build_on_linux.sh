7d71227648c3f0ec16a41f037dcc0ade667b1b1b
Andreas <Andreas-GitHub@protonmail.com>
2018-12-01 14:00:15 +0100

    Add 'build_on_linux.sh'
    Hatseflats, lekker bouwen op linux ;-)

--- demo_gcov/build_on_linux.sh
+++ demo_gcov/build_on_linux.sh
@@ -1,1 +1,24 @@
-todo
+do_build()
+{
+  local cmake_root_dir=.
+  local build_dir="${cmake_root_dir}/build"
+  local release_dir="${build_dir}/release"
+  local test_dir="${build_dir}/unittest"
+  
+  if mkdir -p "${release_dir}" && mkdir -p "${test_dir}";
+  then
+    cd "${release_dir}"
+    
+    cmake -GNinja "${cmake_root_dir}"
+    ninja &                             # no reason to wait for it
+
+    cd "${test_dir}"
+    
+    cmake -GNinja -DUNITTEST=ON "${cmake_root_dir}"
+    ninja
+    ninja test
+    ninja CodeCoverage
+  fi
+}
+  
+do_build
